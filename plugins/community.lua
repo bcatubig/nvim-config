d699a3f5e25bbc1b8a8d032510350ac6b1fb827a
Brandon Catubig <bcatubig@pm.me>
2023-04-05 08:21:06 -0700

    Add plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,15 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.colorscheme.catppuccin" }
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
