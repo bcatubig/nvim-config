1c95637e12ee39700ddc9f09005a6bb5c8c98ae5
Brandon Catubig <bcatubig@pm.me>
2023-04-09 15:03:03 -0700

    updates

--- init.lua
+++ init.lua
@@ -17,7 +17,8 @@ return {
     },
   },
   -- Set colorscheme to use
-  colorscheme = "catppuccin-frappe",
+  -- colorscheme = "catppuccin-frappe",
+  colorscheme = "terafox",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
--- plugins/community.lua
+++ plugins/community.lua
@@ -9,6 +9,7 @@ return {
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.colorscheme.catppuccin" }
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
